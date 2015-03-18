e0f0bdd7cdf902279c113b6ae7ef734431a12c0b
Michael Rokosh <mike.rokosh@gmail.com>
2015-03-18 10:31:48 +0200

    Git repository

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'praxie:geoip-native',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'geoip-native npm wrapper. 4-5 times faster than geoip-lite',
-  git: '',
+  git: 'https://github.com/Praxie/meteor-geoip-native.git',
   documentation: 'README.md'
 });
 
